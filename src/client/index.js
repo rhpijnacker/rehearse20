801f70c7b7bebd8578b7123465bcb6048b4447a5
Ronald Pijnacker <ronald.pijnacker@gmail.com>
2020-05-01 23:37:15 +0200

    electron v1

---
+++ src/client/index.js
@@ -0,0 +1,32 @@
+const electron = require('electron');
+
+const { app, BrowserWindow } = require('electron');
+
+createWindow = () => {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  window.loadFile('index.html');
+};
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
