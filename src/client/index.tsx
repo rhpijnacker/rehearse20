bb6b6eac93c096552c6ef8fbd9f8772f19eb25fa
Ronald Pijnacker <ronald.pijnacker@gmail.com>
2020-05-04 07:53:25 +0200

    Submit on Enter

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -4,19 +4,28 @@ import ReactDOM from 'react-dom';
 const Index = () => {
   const [name, setName] = useState('');
 
-  const onChange = (event) => {
-    setName(event.target.value);
+  const onKeyUp = (event) => {
+    console.log(event.target.value);
+    if (event.key === 'Enter') {
+      navigateToSession();
+    } else {
+      setName(event.target.value);
+    }
   };
 
-  const onClick = () => {
-    location.href = `session.html?name=${name}`;
+  const onClick = () => navigateToSession();
+
+  const navigateToSession = () => {
+    if (name) {
+      location.href = `session.html?name=${name}`;
+    }
   };
 
   return (
     <div>
       <h1>Rehearse 2.0</h1>
       <label>
-        Your name: <input type="text" onChange={onChange}></input>
+        Your name: <input type="text" onKeyUp={onKeyUp}></input>
       </label>
       <button disabled={!name} onClick={onClick}>
         Enter
