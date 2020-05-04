882fdb00120f76fb211af45e73c5acfd5b63ca3a
Ronald Pijnacker <ronald.pijnacker@gmail.com>
2020-05-04 09:03:11 +0200

    Material UI too

--- src/client/session.tsx
+++ src/client/session.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Container, CssBaseline, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
 import './SocketConnection';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  greeting: {},
+}));
 
 const urlParams = new URLSearchParams(window.location.search);
 const name = urlParams.get('name');
 
 const Session = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Rehearse 2.0</h1>
-      <div className="greeting">Hi {name}</div>
-      We are in session!
-    </div>
+    <Container maxWidth="xs">
+      <div className={classes.paper}>
+        <CssBaseline />
+        <Typography component="h1" variant="h5">
+          Rehearse 2.0
+        </Typography>
+        <div className={classes.greeting}>Hi {name}</div>
+        We are in session!
+      </div>
+    </Container>
   );
 };
 
