c5b87d186d47045b0c6819af80685402331c0600
Ronald Pijnacker <ronald.pijnacker@philips.com>
2020-05-09 14:45:31 +0200

    Ping on Windws

--- src/client/ping.ts
+++ src/client/ping.ts
@@ -3,18 +3,32 @@ import child_process from 'child_process';
 export default function ping(target) {
   return new Promise((resolve) => {
     let min, avg, max;
-    const child = child_process.spawn('ping', [target, '-c', '10', '-A']);
+    const args =
+      process.platform === 'win32'
+        ? [target, '-n', '10']
+        : [target, '-c', '10', 'A'];
+    const child = child_process.spawn('ping', args);
     child.stdout.on('data', function (msg) {
       msg = msg.toString();
       console.log(`"${msg}"`);
-      const match = msg.match(
-        /rtt min\/avg\/max\/mdev = ([\d.]+)\/([\d.]+)\/([\d.]+)\/([\d.]+) ms/
-      );
-      if (match) {
-        console.log('match', match)
-        min = parseFloat(match[1]);
-        avg = parseFloat(match[2]);
-        max = parseFloat(match[3]);
+      if (process.platform === 'win32') {
+        const match = msg.match(
+          /Minimum = (\d+)ms, Maximum = (\d+)ms, Average = (\d+)ms/
+        );
+        if (match) {
+          min = parseInt(match[1]);
+          max = parseInt(match[2]);
+          avg = parseInt(match[3]);
+        }
+      } else {
+        const match = msg.match(
+          /rtt min\/avg\/max\/mdev = ([\d.]+)\/([\d.]+)\/([\d.]+)\/([\d.]+) ms/
+        );
+        if (match) {
+          min = parseFloat(match[1]);
+          avg = parseFloat(match[2]);
+          max = parseFloat(match[3]);
+        }
       }
     });
     child.on('close', () => resolve({ min, avg, max }));
